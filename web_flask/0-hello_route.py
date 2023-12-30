8ed7b41cc63765184b65e328e3328b9bd422f863
Josh-dev69 <ebubejoshua69@gmail.com>
2023-12-30 04:36:43 +0100

    Hello Flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
 """
-Start a Flask web application.
-
-Web application listens on 0.0.0.0, port 5000.
-Routes:
-    /: display "Hello HBNB!"
-Use the option strict_slashes=False in route definition.
+Start a Flask web application that display "Hello HBNB!"
 """
 from flask import Flask
 
@@ -13,8 +8,10 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """Display 'Hello HBNB!' on the root route."""
+    """
+    Function to display 'Hello HBNB!' on the root route.
+    """
     return 'Hello HBNB!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run()
